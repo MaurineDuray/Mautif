478192cd0d9544aa4218695d695f64f9d6c53421
MaurineDuray <maurine.duray@skynet.be>
2023-01-18 22:10:00 +0100

    homepage animée

--- assets/js/home.js
+++ assets/js/home.js
@@ -5,10 +5,25 @@ const feuille2 = document.querySelector('#feuille2')
 const feuille3 = document.querySelector('#feuille3')
 const feuille4 = document.querySelector('#feuille4')
 
+const click = document.querySelector('#click')
+
+const more = document.querySelector('#more')
+
 home.addEventListener('click', function(){
     feuille1.style.marginLeft="-20%"
-    feuille2.style.marginRight="-35%"
+    feuille1.style.transform='rotate(10deg)'
+
+    feuille2.style.marginRight="-45vh"
+    feuille2.style.transform='rotate(-10deg)'
+    feuille2.style.top="-100px"
+
     feuille3.style.marginLeft="-15%"
-    feuille4.style.marginRight="-30%"
+    feuille3.style.transform='rotate(-20deg)'
+
+    feuille4.style.marginRight="-25%"
+    feuille4.style.transform='rotate(-20deg)'
+
+    click.style.display="none"
+    more.style.display="block"
 
 })
