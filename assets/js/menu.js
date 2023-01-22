f81532d65a666a60686c766accbaf34123fb3880
MaurineDuray <maurine.duray@skynet.be>
2023-01-22 17:44:56 +0100

    création de pattern _instax

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -4,38 +4,51 @@ const menuResponsive = document.querySelector('#slideMenu')
 const items = document.querySelectorAll('#slideMenu ul li')
 console.log(items)
 
+let search = document.querySelector('#search')
+let input = document.querySelector('#research')
+let loupe = document.querySelector('#search #loupe')
+
+
+// gestion du menu burger 
 burger.addEventListener('click', function(){
     let openned = menuResponsive.getAttribute('class')
     if(openned=='open'){
         menuResponsive.classList.remove('open')
         burger.classList.remove('open');
+        menuResponsive.style.transition="all 1s";
     }else{
         menuResponsive.classList.add('open')
         burger.classList.add('open');
+        menuResponsive.style.transition="all 1s";
     }
    
 })
 
+// gestion de changement de pages menu 
 items.forEach(item=>{
     item.addEventListener('click', function(){
     let openned = menuResponsive.getAttribute('class')
     if(openned=='open'){
         menuResponsive.classList.remove('open')
         burger.classList.remove('open');
+        menuResponsive.style.transition="all 1s";
     }else{
         menuResponsive.classList.add('open')
         burger.classList.add('open');
+        menuResponsive.style.transition="all 1s";
     }
 })
 
-let search = document.querySelector('#search')
-let input = document.querySelector('#research')
-let loupe = document.querySelector('#search #loupe')
+// gestion de la barre de recherche
 
 loupe.addEventListener('click',function(){
     console.log('loupe')
-    input.classList.toggle('actif')
 
+    
+    input.classList.toggle('actif')
+    menuResponsive.classList.remove('open')
+        burger.classList.remove('open');
+        menuResponsive.style.transition="all 1s";
     
 })
 
