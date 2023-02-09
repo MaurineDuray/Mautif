aa6a215241825fdb40256b0e82c5561999e92ba1
MaurineDuray <maurine.duray@skynet.be>
2023-02-09 15:50:47 +0100

    modification des infos du user et de l'avatar

--- src/Controller/AdminUserController.php
+++ src/Controller/AdminUserController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\AvatarModify;
 use App\Entity\User;
 use App\Form\AccountType;
+use App\Form\AvatarModifyType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -32,7 +34,63 @@ class AdminUserController extends AbstractController
         ]);
     }
 
+    /**
+     * Permet d'éditer l'image avatar d'un user
+     */
+    #[Route('admin/user/{slug}/avataredit', name:"avatar_edit")]
+    public function imgEdit(User $user, EntityManagerInterface $manager, Request $request):Response
+    {
+        $imgModify = new AvatarModify();
+        
+        $form = $this->createForm(AvatarModifyType::class, $imgModify);
+        $form->handleRequest($request);
 
+        if($form->isSubmitted() && $form->isValid())
+        {
+            // supprimer l'image dans le dossier
+            if(!empty($user->getAvatar()))
+            {
+                unlink($this->getParameter('uploads_directory').'/'.$user->getAvatar());
+            }
+
+            $file = $form['newAvatar']->getData();
+            if(!empty($file))
+            {
+                $originalFilename = pathinfo($file->getClientOriginalName(),PATHINFO_FILENAME);
+                $safeFilename = transliterator_transliterate('Any-Latin;Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
+                $newFilename = $safeFilename."-".uniqid().".".$file->guessExtension();
+                try{
+                    $file->move(
+                        $this->getParameter('uploads_directory'),
+                        $newFilename
+                    );
+                }catch(FileException $e)
+                {
+                    return $e->getMessage();
+                }
+                $user->setAvatar($newFilename);
+            }
+
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                'Votre avatar a bien été modifié'
+            );
+
+            return $this->redirectToRoute('user_profile',['slug'=>$user->getSlug()]);
+
+        }
+
+        return $this->render("account/avataredit.html.twig",[
+            'myform' => $form->createView()
+        ]);
+    }
+
+    /**
+     * Permet d'éditer les info d'un user
+     */
     #[Route('admin/user/{slug}/edit', name:'account_profile')]
     public function userEdit(User $user, EntityManagerInterface $manager, Request $request):Response
     {
@@ -67,7 +125,7 @@ class AdminUserController extends AbstractController
                 "Votre compte a bien été créé"
             );
 
-            return $this->redirectToRoute('account_login');
+            return $this->redirectToRoute('user_profile',['slug'=>$user->getSlug()]);
 
         }
 
@@ -77,7 +135,7 @@ class AdminUserController extends AbstractController
     }
 
     /**
-     * Permet de supprimer un pattern à partir de l'administration (liste des motifs)
+     * Permet de supprimer un user à partir de l'administration (liste des motifs)
      */
     #[Route('/adminuser/{slug}/delete', name:"admin_user_delete")]
     #[IsGranted('ROLE_ADMIN')]
---
+++ src/Entity/AvatarModify.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Entity;
+
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class AvatarModify
+{
+   
+    private ?int $id = null;
+
+    #[Image(mimeTypes:["image/png", "image/jpeg", "image/jpg"], mimeTypesMessage: "Vous devez charger un fichier jpg, jpeg ou png")]
+    #[Assert\File(maxSize:"1024k", maxSizeMessage: "La taille du fichier est trop grande")]
+    private ?string $newAvatar = null;
+
+    public function getNewAvatar(): ?string
+    {
+        return $this->newAvatar;
+    }
+
+    public function setNewAvatar(string $newAvatar): self
+    {
+        $this->newAvatar = $newAvatar;
+
+        return $this;
+    }
+}
