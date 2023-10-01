3f219c3841f437e1870b8d02b9a1cb52349c4892
MaurineDuray <maurine.duray@skynet.be>
2023-10-01 12:33:50 +0200

    correction css + cookies

--- assets/js/cookies.js
+++ assets/js/cookies.js
@@ -5,6 +5,7 @@ const cookies = document.querySelector(".cookies")
 if (localStorage.getItem('cookies')) {
     cookies.style.display="none"
 }else{
+    cookies.style.display="flex"
     okbtn.addEventListener('click',()=>{
         console.log('cookies oke')
         cookies.style.display="none"
